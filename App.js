e0c10799388a9abd06d8eaff16a82695b54563b1
Emerson Henrique <emerson.hcdossantos@gmail.com>
2023-04-22 17:58:46 -0400

    App Biscoito da Sorte com React Native

--- App.js
+++ App.js
@@ -1,21 +1,89 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { Component } from 'react';
+import { ImageBackground } from 'react-native';
+import { Image, TouchableOpacity } from 'react-native';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class Botao extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.styles = StyleSheet.create({
+      botao: {
+        width: 250,
+        height: 50,
+        borderWidth: 4,
+        borderColor: props.color,
+        backgroundColor: 'black',
+        opacity: 0.8,
+        borderRadius: 25,
+      },
+      btnArea: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center'
+      },
+      btnText: {
+        color: props.color,
+        fontSize: 17,
+        fontWeight: 'bold',
+      },
+    })
+  }
+  render() {
+    return (
+      <TouchableOpacity style={this.styles.botao} onPress={this.props.aoClicar}>
+        <View style={this.styles.btnArea}>
+          <Text style={this.styles.btnText}> {this.props.text}</Text>
+        </View>
+      </TouchableOpacity>
+
+    )
+  }
 }
 
+export default class BiscoitoDaSorte extends Component {
+  constructor(props){
+    super(props);
+    this.state = {texto:'...'};
+    
+    this.frases = ['A vida trará coisas boas se tiveres paciência.', 'Demonstre amor e alegria em todas as oportunidades e verás que a paz nasce dentro de você.', 'Não compense na ira o que lhe falta na razão.', 'Defeitos e virtudes são apenas dois lados da mesma moeda.', 'A maior de todas as torres começa no solo.', 'Não há que ser forte. Há que ser flexível.', 'Gente todo dia arruma os cabelos, por que não o coração?'];
+
+    this.quebrarBiscoito = this.quebrarBiscoito.bind(this);
+
+  }
+  quebrarBiscoito() {
+    let s =this.state;
+    let r = Math.floor(Math.random() * this.frases.length);
+    s.texto = this.frases[r] ;
+    this.setState(s);
+  }
+  render() {
+    return (
+      <ImageBackground source={require('./images/bg.jpg')} style={styles.container}>
+        <Image source={require('./images/cookie.png')} />
+        <Text style={styles.texto}> " {this.state.texto} " </Text>
+        <Botao color="#B59619" text="Quebrar biscoito" aoClicar={this.quebrarBiscoito} />
+        
+      </ImageBackground>
+    );
+  }
+}
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    paddingTop: 20,
     justifyContent: 'center',
+    flex: 1,
+    alignItems: 'center',
+    
+  },
+  texto: {
+    textAlign:'center',
+    fontSize: 22,
+    color: 'white',
+    fontWeight:'bold',
+    fontStyle:'italic',
+    margin: 30,
   },
 });
+
